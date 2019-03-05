7c2b6020b4c32b84d55e19cbedabeb639c893358
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-03-05 12:28:27 -0500

    Adding a PDF/a compliant version of the PDF

---
+++ convert_pdfa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gs -dPDFA -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=aea-pp-2019-abowdschmuttesextonvilhuber-a.pdf aea-pp-2019-abowdschmuttesextonvilhuber.pdf
